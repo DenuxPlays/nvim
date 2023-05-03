1908596f162988d6dd33de3e52f7823a3be68930
Timon Klinkert <timon.klinkert@denux.dev>
2023-05-03 18:35:13 +0200

    Breaked config jdtls not working idk why

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,30 +1,18 @@
 local lsp = require("lsp-zero").preset({})
 
-lsp.preset("recommended")
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
 
-lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'jdtls',
-})
-
--- Fix Undefined global 'vim'
-lsp.nvim_workspace()
-
+lsp.setup()
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = cmp.setup({
 	mapping = {
+		['<CR>'] = cmp.mapping.confirm({select = false}),
 		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-y>'] = cmp.mapping.confirm({ select = true }),
 		["<C-Space>"] = cmp.mapping.complete(),
   	}
-})
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
 })
