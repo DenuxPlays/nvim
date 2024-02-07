a60590e921d21e6aee5bd42b0a36cddd479113f6
Denux <timon.klinkert@denux.dev>
2024-02-07 09:28:45 +0100

    moved to lazy and more lightweight

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = { "windwp/nvim-ts-autotag" },
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"rust",
+				"java",
+				"php",
+			},
+
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
+
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+
+		})
+	end
+}
